36969cc3a3d99a92d74c8bc94869532d8220d826
Bernardo <bernardo-castilho@hotmail.com>
2021-08-25 12:16:13 -0300

    class-based

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import {
     Link
 } from 'react-router-dom';
 
-import { SimulationState, format } from 'simscript';
+import { SimulationState } from 'simscript';
 import { BarberShop } from './simulations/barbershop';
 import { MMC } from './simulations/mmc';
 
@@ -14,11 +14,6 @@ import 'simscript/dist/simscript.css';
 import './App.css';
 
 export default function App() {
-    const sims = {
-        mmc: new MMC(),
-        bshop: new BarberShop()
-    }
-    
     return (
         <BrowserRouter>
             <h1>
@@ -29,6 +24,7 @@ export default function App() {
                     <Link to="/">Home</Link>
                     <Link to="/bshop">Barbershop</Link>
                     <Link to="/mmc">M/M/C</Link>
+                    <Link to="/clock">Clock</Link>
                 </nav>
 
                 {/* 
@@ -40,10 +36,10 @@ export default function App() {
                     <Route path="/bshop">
                         <SimulationCard
                             key={'bshop'}
-                            sim={sims['bshop']}>
+                            sim={new BarberShop()}>
                             <p>
                                 A <a href="https://try-mts.com/gpss-introduction-and-barber-shop-simulation/">
-                                classic GPSS simulation example</a>:
+                                    classic GPSS simulation example</a>:
                                 customers arrive at a barbershop, wait until the barber is available,
                                 get serviced, and leave.</p>
                         </SimulationCard>
@@ -52,7 +48,7 @@ export default function App() {
                     <Route path="/mmc">
                         <SimulationCard
                             key={'mmc'}
-                            sim={sims['mmc']}
+                            sim={new MMC()}
                             name={'M/M/C'}>
                             <p>
                                 A <a href='https://en.wikipedia.org/wiki/M/M/c_queue'>
@@ -62,7 +58,29 @@ export default function App() {
                                 This system is simple enough that there are formulas to calculate
                                 the average queue length and waits
                                 (calculated values are shown in italics).</p>
+                            <p>
+                                time now: <b>???</b>
+                            </p>
                         </SimulationCard>
+                    </Route>
+
+                    <Route path="/clock">
+                        <Clock
+                            key={'clock'}
+                            sim={new MMC()}
+                            name={'MMC (class-based)'}>
+                            <p>
+                                A <a href='https://en.wikipedia.org/wiki/M/M/c_queue'>
+                                classic M/M/C queueing system</a>.
+                                Entities arrive, are served by one of C servers, and leave.</p>
+                            <p>
+                                This system is simple enough that there are formulas to calculate
+                                the average queue length and waits
+                                (calculated values are shown in italics).</p>
+                            <p>
+                                time now: <b>???</b>
+                            </p>
+                        </Clock>
                     </Route>
 
                     <Route path="/">
@@ -93,19 +111,22 @@ function SimulationCard(props: any) {
         outputElement = React.useRef(null),
         runText = String.fromCharCode(9654) + ' Run',
         stopText = String.fromCharCode(9632) + ' Stop';
+
+    // force updates
+    const [, setUpdate] = React.useState({});
+    const forceUpdate = () => setUpdate({});
     
-    // keep track of the simulation state
-    const [simState, setSimState] = React.useState(sim.state);
+    // update output when state changes
     sim.stateChanged.addEventListener(() => {
-        setSimState(sim.state);
+        forceUpdate();
         updateOutput();
     });
 
-    // update output every 500ms
+    // update output every 750ms
     let lastUpdate = 0;
     sim.timeNowChanged.addEventListener(() => {
         let now = Date.now();
-        if (now - lastUpdate > 500) {
+        if (now - lastUpdate > 750) {
             lastUpdate = now;
             updateOutput();
         }
@@ -114,7 +135,7 @@ function SimulationCard(props: any) {
     // handle run/stop button
     const clickRun = (e: any) => {
         lastUpdate = 0;
-        if (simState === SimulationState.Running) {
+        if (sim.state === SimulationState.Running) {
             sim.stop();
         } else {
             sim.start(e.ctrlKey);
@@ -136,11 +157,82 @@ function SimulationCard(props: any) {
         <h2>
             {props.name || sim.name || sim.constructor.name}
         </h2>
-        {props.children}
+        <div>
+            {props.children}
+        </div>
         <button className='btn-run' onClick={clickRun}>
-            {simState !== SimulationState.Running ? runText : stopText}
+            {sim.state !== SimulationState.Running ? runText : stopText}
         </button>
         <div ref={outputElement} />
     </div>;
 }
 
+export class Clock extends React.Component<any, any> {
+    lastUpdate = 0;
+    outputElement = React.createRef<HTMLDivElement>();
+
+    constructor(props: any) {
+        super(props);
+
+        // this is our simulation
+        const sim = props.sim;
+
+        // update output when state changes
+        sim.stateChanged.addEventListener(() => {
+            this.forceUpdate();
+            this.updateOutput();
+        });
+
+        // update output every 750ms
+        sim.timeNowChanged.addEventListener(() => {
+            let now = Date.now();
+            if (now - this.lastUpdate > 750) {
+                this.lastUpdate = now;
+                this.updateOutput();
+            }
+        });
+    }
+
+    // update simulation output
+    updateOutput() {
+        const e = this.outputElement;
+        if (e && e.current) {
+            const
+                sim = this.props.sim,
+                getOutput = this.props.getOutput;
+            e.current.innerHTML = getOutput ? getOutput(sim) : sim.getStatsTable();
+        }
+    }
+
+    // handle run/stop button
+    clickRun(e?: any) {
+        const sim = this.props.sim;
+        this.lastUpdate = 0;
+        if (sim.state === SimulationState.Running) {
+            sim.stop();
+        } else {
+            sim.start(e.ctrlKey);
+        }
+        this.updateOutput();
+    }
+
+    render() {
+        const
+            sim = this.props.sim,
+            runText = String.fromCharCode(9654) + ' Run',
+            stopText = String.fromCharCode(9632) + ' Stop';
+    
+        return <div className='sim-card'>
+            <h2>
+                {this.props.name || sim.name || sim.constructor.name}
+            </h2>
+            <div>
+                {this.props.children}
+            </div>
+            <button className='btn-run' onClick={e => this.clickRun(e)}>
+                {sim.state !== SimulationState.Running ? runText : stopText}
+            </button>
+            <div ref={this.outputElement} />
+        </div>;
+    }
+}
